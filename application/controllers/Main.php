de3a8d5fb9379a58fc3c71225f41acc59a12fcf6
Admin-PC\Admin <k.p.tank.143@gmail.com>
2017-11-18 17:16:24 +0530

    delete some not use code

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -12,16 +12,17 @@ class Main extends CI_Controller {
     }
 
     public function index() {
-        $this->load->view('coming_soon');
+        if (USER_COMING_SOON) {
+            $this->load->view('coming_soon');
+        } else {
+            $data['category'] = generate_array_for_id_object($this->category_model->get_all_category_data(), 'category_id');
+            $data['sub_category'] = generate_array_for_id_object($this->sub_category_model->get_all_sub_category_data(), 'sub_category_id');
+            $data['user_data'] = generate_array_for_id_object($this->login_model->get_all_active_user_data(), 'user_id');
+            $this->load->view('user/common/header', $data);
+            $this->load->view('user/main/main');
+            $this->load->view('user/common/footer');
+            $this->load->view('user/common/backbone_footer');
+        }
     }
 
-//    public function index() {
-//        $data['category'] = generate_array_for_id_object($this->category_model->get_all_category_data(), 'category_id');
-//        $data['sub_category'] = generate_array_for_id_object($this->sub_category_model->get_all_sub_category_data(), 'sub_category_id');
-//        $data['user_data'] = generate_array_for_id_object($this->login_model->get_all_active_user_data(), 'user_id');
-//        $this->load->view('user/common/header', $data);
-//        $this->load->view('user/main/main');
-//        $this->load->view('user/common/footer');
-//        $this->load->view('user/common/backbone_footer');
-//    }
 }
