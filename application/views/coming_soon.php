86e8ccfe1bf59c9ea940d13734fa4f33cd53ac93
Admin-PC\Admin <k.p.tank.143@gmail.com>
2017-11-18 16:10:47 +0530

    -user sid setup

---
+++ application/views/coming_soon.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Inventive Flat Responsive widget Template :: w3layouts</title>
+        <!-- custom-theme -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="Inventive Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+            function hideURLbar(){ window.scrollTo(0,1); } </script>
+        <!-- //custom-theme -->
+        <link href="<?php echo base_url(); ?>css/coming_soon/css/font-awesome.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>css/coming_soon/css/style.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="//fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    </head>
+    <body class="bg-agileinfo">
+        <h1 class="agile-head text-center">Inventive</h1>
+        <div class="container-w3">
+            <div class="w3ls-subhead">
+                <i class="fa fa-soundcloud" aria-hidden="true"></i>
+            </div>
+            <h2>Coming soon!</h2>
+            <div class="demo2"></div>
+            <div class="content2-w3-agileits">
+                <p>Please subscribe to stay tuned!</p>
+                <form action="#" method="post" class="agile-info-form">
+                    <input type="email" class="email" placeholder="Enter your email address" required="">
+                    <input type="submit" value="subscribe!">
+                    <div class="clear"> </div> 
+                </form>	
+            </div>
+            <div class="wthree-social-icons">
+                <ul class="w3-links">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                </ul>
+            </div>
+        </div>	
+        <div class="agileits-w3layouts-copyright text-center">
+            <p>© 2017 Inventive. All rights reserved | Design by <a href="//w3layouts.com/">W3layouts</a></p>
+        </div>
+
+        <!-- js -->
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/coming_soon/js/jquery-2.1.4.min.js"></script>
+        <!-- //js -->
+
+        <link rel="stylesheet" href='<?php echo base_url(); ?>css/coming_soon/css/dscountdown.css' type='text/css' media='all' />
+        <!-- Counter required files -->
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/coming_soon/js/dscountdown.min.js"></script>
+        <script>
+            jQuery(document).ready(function ($) {s
+                $('.demo2').dsCountDown({
+                    endDate: new Date("february 26, 2025 23:59:00"),
+                    theme: 'black'
+                });
+            });
+        </script>
+        <!-- //Counter required files -->
+    </body>
+</html>
